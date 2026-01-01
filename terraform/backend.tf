e33094342ccee5821cc9fd3b6e41e857908c564c
Kevin Mesiab <kmesiab@gmail.com>
2026-01-01 06:01:01 -0800

    Comment out dynamodb_table for bootstrap process
    Updated comments for clarity and corrected command syntax.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,22 +1,22 @@
 # IMPORTANT: Bootstrap Process for First-Time Setup
-# 
-# On first run, terraform init will fail because the DynamoDB table 
+#
+# On first run, terraform init will fail because the DynamoDB table
 # referenced below does not exist yet. Follow this process:
 #
 # 1. Comment out the 'dynamodb_table' line below
 # 2. Run: terraform init
-# 3. Run: terraform apply -target=aws_dynamodb_table.terraform_locks
+# 3. Run: terraform apply --target=aws_dynamodb_table.terraform_locks
 # 4. Uncomment the 'dynamodb_table' line
-# 5. Run: terraform init -reconfigure
+# 5. Run: terraform init --reconfigure
 #
 # See terraform/README.md for detailed bootstrap instructions.
 
 terraform {
   backend "s3" {
-    bucket         = "protein-classifier-terraform-state"
-    key            = "protein-classifier/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "protein-classifier-terraform-locks"
-    encrypt        = true
+    bucket = "protein-classifier-terraform-state"
+    key    = "protein-classifier/terraform.tfstate"
+    region = "us-west-2"
+    # dynamodb_table = "protein-classifier-terraform-locks"
+    encrypt = true
   }
 }
