9799387dcc6d5b224e2389f505987b89e84367b1
Kevin Mesiab <kmesiab@gmail.com>
2026-01-03 17:33:22 -0800

    Enable DynamoDB table for state locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -16,7 +16,7 @@ terraform {
     bucket = "protein-classifier-terraform-state"
     key    = "protein-classifier/terraform.tfstate"
     region = "us-west-2"
-    # dynamodb_table = "protein-classifier-terraform-locks"
+    dynamodb_table = "protein-classifier-terraform-locks"
     encrypt = true
   }
 }
